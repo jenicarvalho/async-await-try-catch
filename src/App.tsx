6c2a02f7641a0cac40fd34687e07f313f39d0755
Jeniffer Carvalho <jenicarvalho@live.com>
2021-02-23 19:55:04 -0300

    aula 31

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import toast, { Toaster } from 'react-hot-toast'
 
 function App() {
+
+  const getUser = async () => {
+      try {
+        const request = await axios.get('http://jsonplaceholder.typicode.com/use332rs')
+        console.log(request.data)
+      } catch(erro) {
+        if(erro.response.status === 404) {
+         toast.error('Não foi possível carregar os usuários')
+        }
+      }
+  }
+
+  useEffect(() => {
+      getUser();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Usuarios</h1>
+      <Toaster />
     </div>
   );
 }
